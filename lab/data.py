d25d8453becb371e0f01c578d8ae959e7aadec79
surister <surister98@gmail.com>
2024-08-11 13:54:35 +0200

    Add data for testing types

---
+++ lab/data.py
@@ -0,0 +1,64 @@
+import datetime
+import uuid
+
+array = {
+    'id': str(uuid.uuid4()),
+    'txt': "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor "
+           "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud "
+           "exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute "
+           "irure dolor",
+    'ip': '127.198.0.1',
+    'i16': -32767,
+    'u16': 32767,
+    'i32': -2147483647,  # CrateDB max value for INTEGER
+    'u32': 2147483647,  # INTEGER
+    'i32_from_text': '-2147483647',
+    'f32': -0.2147481231,
+    'f64': 0.18446744073709551615,
+
+    # > 8byte, MongoDB can only support up to 8 bytes.
+    # 'i64': -18446744073709551615,
+    # 'u64': 18446744073709551615,
+    # 'i128': -1340282366920938463463374607431768211456,
+    # 'u128': 1340282366920938463463374607431768211456,
+
+    'bool': True,
+    'null_': None,
+
+    'datetime': datetime.datetime.now(tz=None),
+    'datetime_2': datetime.datetime.now(tz=datetime.UTC),
+    'obj': {
+        'one': 'two',
+        'three': 4,
+        'five': [6,],
+        'seven': {'eight': 9}
+    },
+    # Missing geo shape.
+
+    # Arrays
+    'empty_array': [],
+    # 'empty_nested_array': [
+    #     [],
+    #     []
+    # ],
+    # 'empty_nested_array_2': [
+    #     [], [1, 2, 3, 4, 5, 6]
+    # ],
+    # 'empty_nested_array_3': [
+    #     [1, 2, 3], [1, 2, 3, 4, 5, 6]
+    # ],
+    # 'empty_nested_array_4': [
+    #     ['Hello', 'World'], [1, 2, 3, 4, 5, 6]
+    # ],
+    # 'empty_nested_array_5': [
+    #     [1, 2, 3], [1, 2, 3, 4, 5, 6]
+    # ],
+    'array_mixed': [12345, "678919"],
+    'vector_float_simple': [0.1],
+    'vector_float': [0.23423423] * 2048,
+    'array_i32': [-123456, -654321],
+    'array_u32': [123456, 654321],
+    'array_text': ["January", "February", "March", "April", "May", "June", "July",
+                   "August", "September", "October", "November", "December"]
+}
+
--- lab/load_data.py
+++ lab/load_data.py
@@ -1,13 +1,47 @@
+import json
 import os
+import pprint
+
 import polars
 import pymongo
+import psycopg2
 import data
 
-def load_row_to_mongo(row: dict, uri: str, table_name: str, db: str = 'test_db') -> None:
+
+def load_row_to_mongo(row: dict,
+                      uri: str,
+                      table_name: str,
+                      db: str = 'testdb',
+                      how_many: int = 1) -> None:
     client = pymongo.MongoClient(uri)
     db = client[db]
     table = db[table_name]
-    table.insert_one(row)
+    # 58.35
+    for _ in range(how_many):
+        table.insert_one(row.copy())
+
+
+def load_row_to_postgres(row: dict,
+                         uri: str,
+                         table_name: str,
+                         db: str = 'testdb',
+                         how_many: int = 1) -> None:
+    columns = ",".join(row.keys())
+    values = ', '.join(['%s'] * len(row))
+
+    conn = psycopg2.connect(uri)
+    curr = conn.cursor()
+    query = f'INSERT INTO {table_name} ({columns}) VALUES ({values})'
+
+    values = list(row.values())
+    for i in values:
+        if isinstance(i, dict):
+            index = values.index(i)
+            values[index] = json.dumps(i)
+
+    curr.executemany(query, ((tuple(values),) * how_many))
+    conn.commit()
+    print('ok')
 
 
 def load_df_to_mongo(df: polars.DataFrame,
@@ -18,7 +52,7 @@ def load_df_to_mongo(df: polars.DataFrame,
     db = client[db]
     table = db[table_name]
 
-    batch_size = 10_000
+    batch_size = 10_00
     buffer = []
     for row in df.iter_rows(named=True):
         buffer.append(row)
@@ -30,6 +64,8 @@ def load_df_to_mongo(df: polars.DataFrame,
 
 df = polars.read_parquet('./data/taxi/')
 MONGO_DB_URI = os.getenv('MONGO_ATLAS_URI')
+POSTGRES_DB_URI = os.getenv('POSTGRES_URI')
 
-load_row_to_mongo(data.arrays, MONGO_DB_URI, 'simple_array')
+# load_row_to_mongo(data.array, 'mongodb://localhost', 'simple_array', how_many=10_0000)
 # load_df_to_mongo(df, MONGO_DB_URI, table_name='taxi')
+# load_row_to_postgres(data.array, POSTGRES_DB_URI, 'simple_array', how_many=10_000)
