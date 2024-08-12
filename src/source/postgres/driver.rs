41b5c1e3bcb77a92cbc2130bafc75c6fa63fb608
surister <surister98@gmail.com>
2024-08-12 16:35:06 +0200

    Refactor Postgres `row_to_normalized_row` with macro `pg2normalized`

--- src/source/postgres/driver.rs
+++ src/source/postgres/driver.rs
@@ -14,7 +14,44 @@ use crate::sink::cratedb::driver::CrateDB;
 use crate::source::mongodb::driver::{NormalizedRow, StringRow};
 use crate::source::source::{Sink, Source};
 
-pub struct PostgresSource {}
+macro_rules! pg2normalized {
+    ($row:expr, $name:expr, $type_name:ty, $result_type:expr) => {
+        {
+            let val: Result<$type_name, sqlx::Error> = $row.try_get($name);
+            let result: NormalizedRow;
+
+                match val {
+                    Ok(v) => {
+                        result = $result_type(v);
+                    }
+                    Err(_) => result = NormalizedRow::None
+                }
+
+            result
+        }
+
+    };
+        ($row:expr, $name:expr, $type_name:ty, $result_type:expr, $to_string:expr) => {
+        {
+            let val: Result<$type_name, Error> = $row.try_get($name);
+            let result: NormalizedRow;
+
+                match val {
+                    Ok(v) => {
+                        result = $result_type($to_string(v));
+                    },
+                    Err(_) => result = NormalizedRow::None
+                }
+
+            result
+        }
+
+    };
+}
+
+pub struct PostgresSource {
+    pub(crate) uri: String
+}
 
 #[async_trait]
 impl Source for PostgresSource {
@@ -28,7 +65,7 @@ impl Source for PostgresSource {
     async fn get_pool(&self) -> Result<Self::PoolType, Self::ErrorType> {
         let pool = PgPoolOptions::new()
             .max_connections(5)
-            .connect("postgres://postgres:postgres@192.168.88.251:5400/postgres")
+            .connect(&*self.uri)
             .await?;
         Ok(pool)
     }
@@ -70,10 +107,14 @@ impl Source for PostgresSource {
 
     async fn migrate_table_to_cratedb(&self, schema: &str, table: &Self::TableType, ignored_columns: Vec<&str>, cratedb: CrateDB, metadata: &mut Metadata) {
         let pool = self.get_pool().await.unwrap();
-
         let mut columns: Vec<String> = vec![];
+        let mut buffer: Vec<Vec<NormalizedRow>> = vec![];
+        let batch_size = 5000;
+        let mut total_documents_sent = 0;
 
-        if !ignored_columns.is_empty(){
+        // Compute the difference between the table's column and `ignored_columns`, we only want to
+        // select the actual data the user wants.
+        if !ignored_columns.is_empty() {
             let one_row_query = format!("SELECT * FROM {}.{} LIMIT 1", &schema, &table);
             let one_row = sqlx::query(&*one_row_query).fetch_one(&pool).await;
             match one_row {
@@ -91,10 +132,6 @@ impl Source for PostgresSource {
         let query_1 = format!("SELECT {} FROM {}.{}", &columns.join(","), &schema, &table);
         let mut rows_stream = sqlx::query(&query_1).fetch(&pool);
 
-        let mut buffer: Vec<Vec<NormalizedRow>> = vec![];
-        let batch_size = 5000;
-        let mut total_documents_sent = 0;
-
         metadata.print_step("Starting connections");
 
         while let Some(row) = rows_stream.next().await {
@@ -110,6 +147,7 @@ impl Source for PostgresSource {
             }
 
             let buffer_len = &buffer.len();
+
             if buffer_len == &batch_size {
                 cratedb.send_batch("doc", &table, &columns, buffer).await;
                 metadata.print_step(format!("Sent batch of {}", buffer_len).as_str());
@@ -131,123 +169,44 @@ impl Source for PostgresSource {
 
             match type_name {
                 "TEXT" => {
-                    let val: Result<String, Error> = row.try_get(name);
-                    match val {
-                        Ok(v) => new_row.push(NormalizedRow::String(v)),
-                        Err(e) => new_row.push(NormalizedRow::None)
-                    }
+                    new_row.push(pg2normalized!(row, name, String, NormalizedRow::String))
                 }
                 "INET" => {
-                    let val: Result<IpAddr, Error> = row.try_get(name);
-                    match val {
-                        Ok(v) => new_row.push(NormalizedRow::String(v.to_string())),
-                        Err(e) => new_row.push(NormalizedRow::None)
-                    }
+                    new_row.push(pg2normalized!(row, name, IpAddr, NormalizedRow::String, |v:IpAddr|v.to_string()))
                 }
                 "INT2" => {
-                    let val: Result<i16, Error> = row.try_get(name);
-                    match val {
-                        Ok(v) => new_row.push(NormalizedRow::I16(v)),
-                        Err(e) => new_row.push(NormalizedRow::None)
-                    }
+                    new_row.push(pg2normalized!(row, name, i16, NormalizedRow::I16))
                 }
-
                 "INT4" => {
-                    let val: Result<i32, Error> = row.try_get(name);
-                    match val {
-                        Ok(v) => new_row.push(NormalizedRow::I32(v)),
-                        Err(e) => new_row.push(NormalizedRow::None)
-                    }
+                    new_row.push(pg2normalized!(row, name, i32, NormalizedRow::I32))
                 }
-
                 "FLOAT4" => {
-                    let val: Result<f32, Error> = row.try_get(name);
-                    match val {
-                        Ok(v) => new_row.push(NormalizedRow::Double32(v)),
-                        Err(e) => new_row.push(NormalizedRow::None)
-                    }
+                    new_row.push(pg2normalized!(row, name, f32, NormalizedRow::Double32))
                 }
                 "FLOAT8" => {
-                    let val: Result<f64, Error> = row.try_get(name);
-                    match val {
-                        Ok(v) => new_row.push(NormalizedRow::Double64(v)),
-                        Err(e) => new_row.push(NormalizedRow::None)
-                    }
+                    new_row.push(pg2normalized!(row, name, f64, NormalizedRow::Double64))
                 }
-
                 "BOOL" => {
-                    let val: Result<bool, Error> = row.try_get(name);
-                    match val {
-                        Ok(v) => new_row.push(NormalizedRow::Bool(v)),
-                        Err(e) => new_row.push(NormalizedRow::None)
-                    }
+                    new_row.push(pg2normalized!(row, name, bool, NormalizedRow::Bool))
                 }
                 "TIMESTAMP" => {
-                    let val: Result<NaiveDateTime, Error> = row.try_get(name);
-                    match val {
-                        Ok(v) => new_row.push(NormalizedRow::String(v.to_string())),
-                        Err(e) => new_row.push(NormalizedRow::None)
-                    }
+                    new_row.push(pg2normalized!(row, name, NaiveDateTime, NormalizedRow::String, |v: NaiveDateTime|v.to_string()));
                 }
-
-                "TIMESTAMPTZ" => {
-                    let val: Result<DateTime<Utc>, Error> = row.try_get(name);
-                    match val {
-                        Ok(v) => new_row.push(NormalizedRow::String(v.to_rfc3339())),
-                        Err(e) => new_row.push(NormalizedRow::None)
-                    }
-                }
-
                 "TEXT[]" => {
-                    let val: Result<Vec<String>, Error> = row.try_get(name);
-                    match val {
-                        Ok(v) => {
-                            new_row.push(NormalizedRow::VecString(v))
-                        },
-                        Err(e) => new_row.push(NormalizedRow::None)
-                    }
+                    new_row.push(pg2normalized!(row, name, Vec<String>, NormalizedRow::VecString));
                 }
-
                 "INT4[]" => {
-                    let val: Result<Vec<i32>, Error> = row.try_get(name);
-                    match val {
-                        Ok(v) => {
-                            new_row.push(NormalizedRow::VecI32(v))
-                        },
-                        Err(e) => new_row.push(NormalizedRow::None)
-                    }
+                    new_row.push(pg2normalized!(row, name, Vec<i32>, NormalizedRow::VecI32));
                 }
-
                 "INT8[]" => {
-                    let val: Result<Vec<i64>, Error> = row.try_get(name);
-                    match val {
-                        Ok(v) => {
-                            new_row.push(NormalizedRow::VecI64(v))
-                        },
-                        Err(e) => new_row.push(NormalizedRow::None)
-                    }
+                    new_row.push(pg2normalized!(row, name, Vec<i64>, NormalizedRow::VecI64));
                 }
-
                 "FLOAT4[]" => {
-                    let val: Result<Vec<f32>, Error> = row.try_get(name);
-                    match val {
-                        Ok(v) => {
-                            new_row.push(NormalizedRow::VecF32(v))
-                        },
-                        Err(e) => new_row.push(NormalizedRow::None)
-                    }
+                    new_row.push(pg2normalized!(row, name, Vec<f32>, NormalizedRow::VecF32));
                 }
-
                 "FLOAT8[]" => {
-                    let val: Result<Vec<f64>, Error> = row.try_get(name);
-                    match val {
-                        Ok(v) => {
-                            new_row.push(NormalizedRow::VecF64(v))
-                        },
-                        Err(e) => new_row.push(NormalizedRow::None)
-                    }
+                    new_row.push(pg2normalized!(row, name, Vec<f64>, NormalizedRow::VecF64))
                 }
-
                 _ => {
                     let val = row.try_get(name);
                     match val {
